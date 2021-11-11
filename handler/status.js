ec9e356760ab6eea24b5792ae96e74d09ca3848d
EnzDefv <94133670+EnzDefv@users.noreply.github.com>
2021-11-11 19:03:42 +0100

    status.js

---
+++ handler/status.js
@@ -0,0 +1,23 @@
+module.exports = (bot) => {
+
+    bot.status({
+
+        text: "GITHUB",
+
+        type: "PLAYING",
+
+        time: 12
+
+    })
+
+    bot.status({
+
+        text: "enzdefv",
+
+        type: "PLAYING",
+
+        time: 12
+
+    })
+
+}
